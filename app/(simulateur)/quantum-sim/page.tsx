ee379158fed97938f5b5643ad8e81a6a41651334
maximedervaux <maximeder.pro@gmail.com>
2026-02-06 10:21:37 +0100

    fix: CSS problemes

--- app/(simulateur)/quantum-sim/page.tsx
+++ app/(simulateur)/quantum-sim/page.tsx
@@ -6,17 +6,20 @@ import DashBoard from "./components/DashBoard/DashBoard";
 import { PythonWorkerProvider } from "../core/contexts/PythonWorkerContext";
 
 export default function Simulateur() {
-
   return (
     <PythonWorkerProvider>
-      <div className="content">
-        <div className="headerBar">
-          <Link href="/"><img src="/logo.png" alt="Quantum Sim Logo" width={80} height={40} /></Link>
-          <NavigationTabs />
-        </div>
+      <div className="simulatorRoot">
+        <div className="content">
+          <div className="headerBar">
+            <Link href="/">
+              <img src="/logo.png" alt="Quantum Sim Logo" width={80} height={40} />
+            </Link>
+            <NavigationTabs />
+          </div>
 
-        <DashBoard />
-      </div> 
+          <DashBoard />
+        </div>
+      </div>
     </PythonWorkerProvider>
   );
 }
