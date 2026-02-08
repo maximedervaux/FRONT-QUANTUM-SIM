647db2b8bab556402514288e8af026f56410ae38
Williampasset <william.passet@student.junia.com>
2026-02-08 22:17:23 +0100

    fix(landing): remove useless gsap call

--- app/(landing)/page.tsx
+++ app/(landing)/page.tsx
@@ -1,15 +1,11 @@
 "use client";
 
 import { useRef, useState } from "react";
-import gsap from "gsap";
-import { ScrollTrigger } from "gsap/ScrollTrigger";
 import './styles/style.css'
 import Bento from "./components/Bento/Bento";
 import Hero from "./components/Hero/Hero";
 import Notebook from "./components/Notebook/Notebook";
 import { useGsapAnimations } from "./hooks/useGsapAnimation";
-
-gsap.registerPlugin(ScrollTrigger);
 
 export default function Home() {
   const heroRef = useRef<HTMLDivElement>(null);
