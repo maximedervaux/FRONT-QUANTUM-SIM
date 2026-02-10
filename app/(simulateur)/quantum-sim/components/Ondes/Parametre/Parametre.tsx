61da7d1f49f63938d45bbe74f6ecac0bb861abe7
maximedervaux <maximeder.pro@gmail.com>
2026-02-10 15:53:42 +0100

    feat(parametre): add parameters

--- app/(simulateur)/quantum-sim/components/Ondes/Parametre/Parametre.tsx
+++ app/(simulateur)/quantum-sim/components/Ondes/Parametre/Parametre.tsx
@@ -1,6 +1,9 @@
 import style from "./Parametre.module.css";
 import { Slider } from "@/components/ui/slider"
 import { useWaveStore } from "../../../store/onde.store";
+import { NativeSelect, NativeSelectOption } from "@/components/ui/native-select";
+import { useState } from "react";
+import { Input } from "@/components/ui/input";
 
 
 
@@ -9,13 +12,25 @@ export default function Parametre( ) {
     amplitude,
     phase,
     setAmplitude,
-    setPhase
+    setPhase,
+    setFunction
   } = useWaveStore();
 
+  // TODO Useref temp
+   const [longueur, setLongueur] = useState(0.01);
+   const [harmonique, setHarmonique] = useState(0.01);
     
     return (
         <div className={style.parametre}>
             <h1>Paramètres de l'onde</h1>
+            <div className={style.selectContainer}>
+                <NativeSelect defaultValue="" onChange={(value) => setFunction(value.target.value)}>
+                        <NativeSelectOption value="">Fonction d'onde</NativeSelectOption>
+                        <NativeSelectOption value="gaussienne">Gaussienne</NativeSelectOption>
+                        <NativeSelectOption value="sinusoidale">Sinusoidale</NativeSelectOption>
+                </NativeSelect>
+
+            </div>
             <div className={style.sliderContainer}>
                 <p>Amplitude : {amplitude}</p>
                 <Slider
@@ -27,15 +42,37 @@ export default function Parametre( ) {
                 />
             </div>
             <div className={style.sliderContainer}>
-                    <p>Phase : {phase}</p>
+                    <p>Phase : {phase} π</p>
                     <Slider
                         defaultValue={[phase]}
                         min={0.01}
-                        max={2 * Math.PI}
+                        max={2}
                     step={0.01}
                     onValueChange={(value) => setPhase(value[0])}
                 />
             </div>
+            <div className={style.inputContainer}>
+                    <p>Longueur d'onde</p>
+                    <Input
+                        placeholder="0"
+                        defaultValue={longueur}
+                        onChange={(value) => setLongueur(value.target.valueAsNumber)    
+                    }
+                />
+                λ
+            </div>
+              <div className={style.inputContainer}>
+                    <p>Harmonique</p>
+                    <Input
+                        placeholder="0"
+                        defaultValue={harmonique}
+                        onChange={(value) => setHarmonique(value.target.valueAsNumber)    
+                    }
+                />
+                
+            </div>
+
+            
         </div>
     );
 }
--- app/(simulateur)/quantum-sim/store/onde.store.tsx
+++ app/(simulateur)/quantum-sim/store/onde.store.tsx
@@ -3,9 +3,11 @@ import { create } from "zustand";
 interface WaveState {
   amplitude: number;
   phase: number;
+  
 
   setAmplitude: (value: number) => void;
   setPhase: (value: number) => void;
+  setFunction: (value: string) => void;
 }
 
 export const useWaveStore = create<WaveState>((set) => ({
@@ -14,4 +16,16 @@ export const useWaveStore = create<WaveState>((set) => ({
 
   setAmplitude: (value) => set({ amplitude: value }),
   setPhase: (value) => set({ phase: value }),
+  setFunction: (value) => {
+    switch (value) {
+      case "gaussienne":
+        set({ phase: 0.1 , amplitude: 1 });
+        break;
+      case "sinusoidale":
+        set({ phase: 0.005 , amplitude: 1 });
+        break;
+      default:
+        set({ phase: 0 , amplitude: 0 });
+    }
+  },
 }));
