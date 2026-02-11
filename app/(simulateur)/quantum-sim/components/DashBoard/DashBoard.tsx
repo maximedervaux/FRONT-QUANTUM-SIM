83a24ee3ec1c779f513e83447cea13b91651605c
maximedervaux <maximeder.pro@gmail.com>
2026-02-11 16:24:25 +0100

    feat(dashboard): add 3D chart component to enhance visualization

--- app/(simulateur)/quantum-sim/components/DashBoard/DashBoard.tsx
+++ app/(simulateur)/quantum-sim/components/DashBoard/DashBoard.tsx
@@ -2,6 +2,7 @@ import Chart from "../Ondes/2DChart/Chart";
 import styles from "./DashBoard.module.css";
 import Parametre from "../Ondes/Parametre/Parametre";
 import { useNavigationStore } from "../../store/navigation.store";
+import ThreeChart from "../Ondes/3DChart/ThreeChart";
 
 export default function DashBoard() {
 
@@ -11,7 +12,8 @@ export default function DashBoard() {
     return (
                     <div className={styles.dashboard}>
                         <div className={styles.chartsContainer}>
-                                   {activePage === "ondes" && <Chart />}
+                                   {activePage === "ondes" && <div style={{ display: 'flex', flexDirection: 'column' , margin: '0px', padding: '0px', width: '100%' , height: '100%' }}><Chart /> <ThreeChart/></div>}
+
                        
 
                         </div>
---
+++ app/(simulateur)/quantum-sim/components/Ondes/3DChart/ThreeChart.tsx
@@ -0,0 +1,11 @@
+
+
+
+export default function ThreeChart() {
+    return (
+        <div>
+        <h1>3D Chart</h1>
+        {/* Intégrer votre composant de visualisation 3D ici */}
+        </div>
+    );
+    }
