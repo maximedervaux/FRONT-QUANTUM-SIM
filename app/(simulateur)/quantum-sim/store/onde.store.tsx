abfb5a7032e124cfbc773bdaab15adf965b622f3
Williampasset <william.passet@student.junia.com>
2026-03-21 14:46:29 +0100

    fix(style): many fix on styles to fix display issues and delete animation input for phase

--- app/(simulateur)/quantum-sim/store/onde.store.tsx
+++ app/(simulateur)/quantum-sim/store/onde.store.tsx
@@ -1,60 +1,58 @@
-import { create } from "zustand";
+import { create } from 'zustand';
 
 interface WaveState {
-  amplitude: number;
-  phase: number;
-  harmonics: number;
-  wavelength: number;
-  period: number;
-  time: number;
-  isAnimatingPhase: boolean;
-  isAnimatingTime: boolean;
+	amplitude: number;
+	phase: number;
+	harmonics: number;
+	wavelength: number;
+	period: number;
+	time: number;
+	isAnimatingTime: boolean;
 
-  setAmplitude: (value: number) => void;
-  setPhase: (value: number) => void;
-  setFunction: (value: string) => void;
-  setHarmonics: (value: number) => void;
-  setWavelength: (value: number) => void;
-  setPeriod: (value: number) => void;
-  setTime: (value: number) => void;
-  toggleAnimationPhase: () => void;
-  toggleAnimationTime: () => void;
-  resetPhase: () => void;
-  resetTime: () => void;
+	setAmplitude: (value: number) => void;
+	setPhase: (value: number) => void;
+	setFunction: (value: string) => void;
+	setHarmonics: (value: number) => void;
+	setWavelength: (value: number) => void;
+	setPeriod: (value: number) => void;
+	setTime: (value: number) => void;
+	toggleAnimationTime: () => void;
+	resetPhase: () => void;
+	resetTime: () => void;
 }
 
-export const useWaveStore = create<WaveState>((set) => ({
-  amplitude: 0,
-  phase: 0,
-  harmonics: 1,
-  wavelength: 1,
-  period: 3,
-  time: 0,
-  isAnimatingPhase: false,
-  isAnimatingTime: false,
+export const useWaveStore = create<WaveState>(set => ({
+	amplitude: 0,
+	phase: 0,
+	harmonics: 1,
+	wavelength: 1,
+	period: 3,
+	time: 0,
+	isAnimatingPhase: false,
+	isAnimatingTime: false,
 
-  setAmplitude: (value) => set({ amplitude: value }),
-  setPhase: (value) => set((state) => ({
-    phase: (state.phase + value) % (2 * Math.PI) // Modulo 2π pour boucler
-  })),
-  setFunction: (value) => {
-    switch (value) {
-      case "gaussienne":
-        set({ phase: 0.1 , amplitude: 1 });
-        break;
-      case "sinusoidale":
-        set({ phase: 0.005 , amplitude: 1 });
-        break;
-      default:
-        set({ phase: 0 , amplitude: 0 });
-    }
-  },
-  setHarmonics: (value) => set({harmonics: value}),
-  setWavelength: (value) => set({wavelength: value}),
-  setPeriod: (value) => set({period: value}),
-  setTime: (value) => set((state) => ({ time: state.time + value })),
-  toggleAnimationPhase: () => set((state) => ({ isAnimatingPhase: !state.isAnimatingPhase })),
-  toggleAnimationTime: () => set((state) => ({ isAnimatingTime: !state.isAnimatingTime })),
-  resetPhase: () => set({ phase: 0 }),
-  resetTime: () => set({ time: 0 }),
+	setAmplitude: value => set({ amplitude: value }),
+	setPhase: value =>
+		set(state => ({
+			phase: (state.phase + value) % (2 * Math.PI),
+		})),
+	setFunction: value => {
+		switch (value) {
+			case 'gaussian':
+				set({ phase: 0.1, amplitude: 1 });
+				break;
+			case 'sinus':
+				set({ phase: 0.005, amplitude: 1 });
+				break;
+			default:
+				set({ phase: 0, amplitude: 0 });
+		}
+	},
+	setHarmonics: value => set({ harmonics: value }),
+	setWavelength: value => set({ wavelength: value }),
+	setPeriod: value => set({ period: value }),
+	setTime: value => set(state => ({ time: state.time + value })),
+	toggleAnimationTime: () => set(state => ({ isAnimatingTime: !state.isAnimatingTime })),
+	resetPhase: () => set({ phase: 0 }),
+	resetTime: () => set({ time: 0 }),
 }));
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,44 +1,42 @@
-import type { Metadata } from "next";
-import "./styles/globals.css";
+import type { Metadata } from 'next';
+import './styles/globals.css';
 import localFont from 'next/font/local';
 
 const tiltWarp = localFont({
-  src: [
-    {
-      path: './assets/fonts/TiltWarp.ttf',
-      weight: '400',
-      style: 'normal',
-    },
-  ],
-  variable: '--font-tilt-warp', 
+	src: [
+		{
+			path: './assets/fonts/TiltWarp.ttf',
+			weight: '400',
+			style: 'normal',
+		},
+	],
+	variable: '--font-tilt-warp',
 });
 
 const lato = localFont({
-  src: [
-    { 
-      path : './assets/fonts/Lato/Lato-Regular.ttf', 
-      weight: '400', 
-      style: 'normal'
-    }],
-  variable: '--font-lato', 
+	src: [
+		{
+			path: './assets/fonts/Lato/Lato-Regular.ttf',
+			weight: '400',
+			style: 'normal',
+		},
+	],
+	variable: '--font-lato',
 });
 
-
 export const metadata: Metadata = {
-title: "Quantum Sim",
-  description: "Web application designed to demonstrate the capabilities of our Python library",
+	title: 'Quantum Sim',
+	description: 'Web application designed to demonstrate the capabilities of our Python library',
 };
 
 export default function RootLayout({
-  children,
+	children,
 }: Readonly<{
-  children: React.ReactNode;
+	children: React.ReactNode;
 }>) {
-  return (
-    <html lang="fr" className={`${tiltWarp.variable} ${lato.variable}`}>
-      <body>
-        {children}
-      </body>
-    </html>
-  );
+	return (
+		<html lang="fr" className={`${tiltWarp.variable} ${lato.variable}`}>
+			<body>{children}</body>
+		</html>
+	);
 }
